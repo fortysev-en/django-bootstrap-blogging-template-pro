6c773542cc5f194211407de6d8bfd9e5b1fdb731
Pushkar Jadhao <pushkar.jadhao@gmail.com>
2022-03-23 22:49:27 +0530

    successful google captcha integration

--- blogs/views_api.py
+++ blogs/views_api.py
@@ -54,40 +54,41 @@ class SignupView(APIView):
         response['status'] = 500
         response['message'] = 'Something went wrong!'
 
-        dataRe = request.data
-        recaptcha_response = dataRe.get('gReCaptcha')
-        data = {
+        data = request.data
+
+        recaptcha_response = data.get('gReCaptcha')
+        recaptchaData = {
         'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
         'response': recaptcha_response
         }
-        r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
+        r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=recaptchaData)
         result = r.json()
 
-        print(result)
+        if result['success']:        
+            try:
+                check_user = User.objects.filter(username = data.get('username')).first()
+                check_email = User.objects.filter(email = data.get('username')).first()
 
-        
-        # try:
-        #     data = request.data
-        #     check_user = User.objects.filter(username = data.get('username')).first()
-        #     check_email = User.objects.filter(email = data.get('username')).first()
+                if check_user:
+                    response['message'] = 'User already exist!'
+                    raise Exception('User already exist!')
+                elif check_email:
+                    response['message'] = 'Email already exist!'
+                    raise Exception('Email already exist!')
+                else:
+                    usr = User.objects.create_user(first_name = data.get('firstname'), last_name = data.get('lastname'), email = data.get('email'), username = data.get('username'), password = data.get('password'))
 
-        #     if check_user:
-        #         response['message'] = 'User already exist!'
-        #         raise Exception('User already exist!')
-        #     elif check_email:
-        #         response['message'] = 'Email already exist!'
-        #         raise Exception('Email already exist!')
-        #     else:
-        #         usr = User.objects.create_user(first_name = data.get('firstname'), last_name = data.get('lastname'), email = data.get('email'), username = data.get('username'), password = data.get('password'))
+                    usr.is_active = False
+                    send_email(usr)
 
-        #         usr.is_active = False
-        #         send_email(usr)
-
-        #         response['message'] = f'Registration Successfull <b>{data.get("firstname")}</b>! Please verify your email address before using the service!'
-        #         response['status'] = 200
-        
-        # except Exception as e:
-        #     print(e)
+                    response['message'] = f'Registration Successfull <b>{data.get("firstname")}</b>! Please verify your email address before using the service!'
+                    response['status'] = 200
+            
+            except Exception as e:
+                print(e)
+        else:
+            alert = result['error-codes'][0]
+            response['message'] = 'Captcha Error: ' + alert
 
         return Response(response)
 
