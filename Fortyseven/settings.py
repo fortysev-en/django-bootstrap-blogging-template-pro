6beaf9af48d34956ed93d224486558899cdb7385
Pushkar Jadhao <pushkar.jadhao@gmail.com>
2022-04-03 12:23:05 +0530

    Update settings.py

--- Fortyseven/settings.py
+++ Fortyseven/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static"]
 
