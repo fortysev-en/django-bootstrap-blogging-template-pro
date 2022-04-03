4c1750047c86776fc3cc2b01d0545b0f147018cd
Pushkar Jadhao <pushkar.jadhao@gmail.com>
2022-04-03 12:06:38 +0530

    Add files via upload

--- Fortyseven/settings.py
+++ Fortyseven/settings.py
@@ -46,10 +46,11 @@ INSTALLED_APPS = [
     'blogs'
 ]
 
-CSRF_TRUSTED_ORIGINS = ['https://*.ngrok.io','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://*.ngrok.io','https://127.0.0.1']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static"]
 
