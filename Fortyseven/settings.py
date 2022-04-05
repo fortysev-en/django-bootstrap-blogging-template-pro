309546c7442c1f4ebe1da746456e25b1d6763dbe
Pushkar Jadhao <pushkar.jadhao@gmail.com>
2022-04-05 20:38:32 +0530

    fixed static files url for debug=true

--- Fortyseven/settings.py
+++ Fortyseven/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fe1#n^faux117p6=ip64q0%)f1^6sb3sdw6&2&1b)w77ho3nfy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -126,14 +126,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
-# STATICFILES_DIR = {
-#     os.path.join(BASE_DIR , "static")
-# }
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / "static"]
 
-# MEDIA_ROOT = os.path.join(BASE_DIR , 'static')
-# MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR , 'static')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -169,16 +168,16 @@ EMAIL_USE_TLS = True
 
 
 # ========= AWS CONFIG ==============
-AWS_ACCESS_KEY_ID = 'AKIAV34PMTYUDHPCKX4U'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'fortyseven-blog'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
-AWS_S3_ENDPOINT_URL = 'https://s3-accelerate.amazonaws.com'
-AWS_S3_SIGNATURE_VERSION ='s3v4'
-AWS_S3_REGION_NAME = 'us-east-1'
+# AWS_ACCESS_KEY_ID = 'AKIAV34PMTYUDHPCKX4U'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'fortyseven-blog'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = None
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+# AWS_S3_ENDPOINT_URL = 'https://s3-accelerate.amazonaws.com'
+# AWS_S3_SIGNATURE_VERSION ='s3v4'
+# AWS_S3_REGION_NAME = 'us-east-1'
 
 '''
 ====BUCKET POLICY====
--- staticfiles/js/marquee-config.js
+++
@@ -1,7 +0,0 @@
-const root = document.documentElement;
-const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
-const marqueeContent = document.querySelector("ul.marquee-content");
-root.style.setProperty("--marquee-elements", marqueeContent.children.length);
-for (let i = 0; i < marqueeElementsDisplayed; i++) {
-marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
-}
